da4d6dfa12a3bff88ebdd2dc1297babb62f99687
bandid <dannimation@gmail.com>
2014-09-16 15:21:54 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Harvey
+ */
+?>
+
+</div>	
+</div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+		</div><!-- close .row -->
+	</div><!-- close .container -->
+</div><!-- close .main-content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="container">
+		<div class="row">
+			<div class="site-footer-inner col-sm-12">
+
+				<div class="site-info">
+					<?php do_action( 'Harvey_credits' ); ?>
+					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_tk' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'Harvey' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( __( 'Theme: %1$s by %2$s.', 'Harvey' ), 'Harvey', '<a href="http://wpup.co/" rel="designer">wpup</a>' ); ?>
+				</div><!-- close .site-info -->
+
+			</div>
+		</div>
+	</div><!-- close .container -->
+</footer><!-- close #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
